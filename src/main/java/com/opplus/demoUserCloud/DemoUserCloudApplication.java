c4c8fbc216b4f35fbfe4dfbdbae0c28ceb2f5646
sandraLoz <54351588+sandraLoz@users.noreply.github.com>
2022-06-01 13:18:57 +0200

    init

---
+++ src/main/java/com/opplus/demoUserCloud/DemoUserCloudApplication.java
@@ -0,0 +1,13 @@
+package com.opplus.demoUserCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUserCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUserCloudApplication.class, args);
+	}
+
+}
